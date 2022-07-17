870fd49c671dcc38b8f6f79ee9ab9ff27500c55d
eimamagi <emir.imamagic@gmail.com>
2022-07-17 16:21:24 +0200

    Added modules for mounting BeeGFS without packages or services. (#12)
    * Added modules for mounting BeeGFS without helperd.
    
    * Don't install any BeeGFS packages on thin client.

---
+++ manifests/client_thin.pp
@@ -0,0 +1,41 @@
+#
+# = Class: beegfs::client_thin
+#
+# This module manages BeeGFS thin client. Sets mounts
+# without installing beegfs-client and beegfs-helpers.
+# Mountpoints are defined with beegfs::mount_thin resource.
+#
+
+class beegfs::client_thin (
+  $kernel_module     = "puppet:///modules/beegfs/${::kernelrelease}/${::beegfsversion}/${rdma_path}/beegfs.ko",
+  $beegfs_mount_hash,
+) {
+  file { [ "/lib/modules/${::kernelrelease}/updates/fs", "/lib/modules/${::kernelrelease}/updates/fs/beegfs_autobuild" ]:
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+  }
+
+  file { "/lib/modules/${::kernelrelease}/updates/fs/beegfs_autobuild/beegfs.ko":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => $kernel_module,
+    require => [ File["/lib/modules/${::kernelrelease}/updates/fs/beegfs_autobuild"] ],
+  }
+
+  exec { 'load_module':
+    command => "/sbin/depmod -a && touch /etc/beegfs/depmod_${::kernelrelease}_${::beegfsversion}",
+    path    => ['/sbin', '/bin', '/usr/sbin', '/usr/bin'],
+    creates => "/etc/beegfs/depmod_${::kernelrelease}_${::beegfsversion}",
+    require => [ File["/lib/modules/${::kernelrelease}/updates/fs/beegfs_autobuild/beegfs.ko"] ],
+  }
+
+  kmod::load { 'beegfs': }
+
+  if $beegfs_mount_hash {
+    create_resources('beegfs::mount_thin', $beegfs_mount_hash)
+  }
+}
---
+++ manifests/mount_thin.pp
@@ -0,0 +1,29 @@
+# Define: beegfs::mount_thin
+#
+# This module manages BeeGFS mount
+#
+define beegfs::mount_thin ($cfg, $mnt, $cfg_source) {
+  file { $mnt:
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+  }
+  file { "/etc/beegfs/${cfg}":
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => $cfg_source,
+  }
+  mount { $mnt:
+    ensure  => mounted,
+    atboot  => true,
+    device  => 'beegfs_nodev',
+    fstype  => 'beegfs',
+    options => "rw,relatime,cfgFile=${cfg},_netdev",
+    dump    => 0,
+    pass    => 0,
+    require => [ File[$mnt], File["/etc/beegfs/${cfg}"] ],
+  }
+}
