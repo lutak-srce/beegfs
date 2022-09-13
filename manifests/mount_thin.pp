4442475f40dc96e1691da8091cfc6bbb2104a7bb
eimamagi <emir.imamagic@gmail.com>
2022-09-13 20:30:20 +0200

    Add support for authn file and alternative client template.

--- manifests/mount_thin.pp
+++ manifests/mount_thin.pp
@@ -2,12 +2,21 @@
 #
 # This module manages BeeGFS mount
 #
-define beegfs::mount_thin ($cfg, $mnt, $cfg_source) {
+define beegfs::mount_thin ($cfg, $mnt, $cfg_source, $authn) {
   file { $mnt:
     ensure  => directory,
     owner   => root,
     group   => root,
     mode    => '0755',
+  }
+  if ( $authn ) {
+    file { "/etc/beegfs/connauthfile-${title}":
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0600',
+      content => $authn,
+    }
   }
   file { "/etc/beegfs/${cfg}":
     ensure  => file,
