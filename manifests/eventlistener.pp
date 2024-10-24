b8f73bc92f7d2a7f2bfcc41bf9e1be0fc8d28aee
eimamagi <emir.imamagic@gmail.com>
2024-10-24 03:04:15 +0200

    Add BeeGFS Hive support.

---
+++ manifests/eventlistener.pp
@@ -0,0 +1,24 @@
+# Class: beegfs::hive
+#
+# This module manages BeeGFS Event Listener
+#
+class beegfs::eventlistener (
+  $enable                = false,
+  $file_event_log_target = '',
+  $client_addr           = '',
+  $update_port           = 9000,
+  $bee_update_debug      = 0,
+) inherits beegfs {
+  file { '/etc/beegfs/beegfs-eventlistener.conf':
+    require => Package['beegfs-utils'],
+    content => template('beegfs/beegfs-eventlistener.conf.erb'),
+  }
+
+  if ( $enable ) {
+    service { 'beegfs-eventlistener':
+      ensure    => running,
+      enable    => true,
+      require   => [ Package['beegfs-hive-index'], File['/etc/beegfs/beegfs-eventlistener.conf'] ],
+    }
+  }
+}
---
+++ manifests/hive.pp
@@ -0,0 +1,44 @@
+# Class: beegfs::hive
+#
+# This module manages BeeGFS Hive Index
+#
+class beegfs::hive (
+  $enable     = false,
+  $threads    = 2,
+  $index_path = '',
+  $mount_path = '',
+  $port       = 9000,
+  $debug      = 0,
+  
+) inherits beegfs {
+  include yum::repo::beegfs::hive
+
+  package { 'beegfs-hive-index':
+    ensure => $version,
+  }
+
+  if ( $index_path != '' ) {
+    $index_paths = "${mount_path}:${index_path}"
+    $idx_path = $index_path
+  } else {
+    $index_paths = $mount_path
+    $idx_path = $mount_path
+  }
+
+  file { '/etc/beegfs/index/config':
+    require => Package['beegfs-hive-index'],
+    content => template('beegfs/beegfs-hive-config.erb'),
+  }
+
+  if ( $enable ) {
+    file { '/etc/beegfs/index/updateEnv.conf':
+      require => Package['beegfs-hive-index'],
+    }
+      
+    service { 'bee-update':
+      ensure    => running,
+      enable    => true,
+      require   => [ Package['beegfs-hive-index'], File['/etc/beegfs/index/updateEnv.conf'] ],
+    }
+  }
+}
